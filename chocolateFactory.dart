5fbfb4c9f67c56f11629d159d54b89a2183d159e
Robin <daorob@gmail.com>
2020-01-21 11:13:55 -0600

    Create the cake Baker to inspect the cake type and add logic to decide on what to output.

--- chocolateFactory.dart
+++ chocolateFactory.dart
@@ -12,8 +12,19 @@ void main() {
 
   final order = new Order('banana');
 
+  final baker = new StreamTransformer.fromHandlers(
+    handleData: (cakeType, sink) {
+      if (cakeType == 'chocolate') {
+        sink.add(new Cake());
+      } else {
+        sink.addError('I cant bake that type!!!');
+      }
+    }
+  );
+
   controller.sink.add(order);
 
   controller.stream
     .map((order) => order.type)
+    .transform(baker)
 }
